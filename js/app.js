756049de87b46eda0efae09e059cb58cf5392507
anniebae <bae.annie@gmail.com>
2015-07-01 22:28:29 -0400

    products.js file created to remove repetitive directive patterns

--- js/app.js
+++ js/app.js
@@ -21,28 +21,7 @@ app.controller("ReviewController", function() {
   }
 });
 
-app.directive('productTitle', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'product-title.html'
-  };
-});
 
-app.directive('productPanels', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'product-panels.html',
-    controller: function() {
-      this.tab = 1;
-      this.selectTab = function(setTab) {
-        this.tab = setTab;
-      };
-      this.isSelected = function(checkTab) {
-        return this.tab == checkTab;
-      };
-    }
-  };
-});
 
 var gems = [
   {
