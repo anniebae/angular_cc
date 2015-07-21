bb1525d4a1a879573ce15d9e36117e6996f7a25a
anniebae <bae.annie@gmail.com>
2015-07-20 22:18:56 -0400

    StoreController created

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('store', []);
+(function() {
+
+  var app = angular.module('store', []);
+
+  // controllers are where we define our app's behavior by defining functions and values
+  app.controller("StoreController", function() {
+    this.product = gem;
+  });
+
+  var gem = {
+    name: 'Dodecahedron',
+    price: 2.95,
+    description: "......",
+  },
+
+});
