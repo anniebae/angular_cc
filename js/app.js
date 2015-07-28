adbc5f7bd5f9bb63ef6cf3c0c46bd247a2ce3c8b
anniebae <bae.annie@gmail.com>
2015-07-27 21:29:59 -0400

    isSelected function created for comparison

--- js/app.js
+++ js/app.js
@@ -17,6 +17,9 @@
     this.selectTab = function(setTab) {
       this.tab = setTab;
     };
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    }
   });
 
   var gems = [
